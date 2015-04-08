e7ac201c3ff52138bdc1f345bebffc565d224b31
FakeShemp <sakcheen@gmail.com>
2015-04-08 12:17:56 +0200

    Initial commit of Maya export
    Add dummy project which does nothing.

---
+++ Maya/MayaExport/main.cpp
@@ -0,0 +1,28 @@
+#include "maya_includes.h"
+#include <iostream>
+
+using namespace std;
+
+EXPORT MStatus initializePlugin(MObject obj)
+{
+	MStatus res = MS::kSuccess;
+
+	MFnPlugin myPlugin(obj, "Maya plugin", "1.0", "Any", &res);
+	if (MFAIL(res))
+	{
+		CHECK_MSTATUS(res);
+	}
+
+	MGlobal::displayInfo("Maya plugin loaded!");
+
+	return res;
+}
+
+EXPORT MStatus uninitializePlugin(MObject obj)
+{
+	MFnPlugin plugin(obj);
+
+	MGlobal::displayInfo("Maya plugin unloaded!");
+
+	return MS::kSuccess;
+}
