b0afb78fea943fb1075bd32993d153ab3f62250b
maqu13 <magqua@gmail.com>
2015-04-11 13:49:37 +0200

    Revert "Camera Inc."
    This reverts commit 460cf8887761f1a2c0bdde458dcf3e8ebc337cbe.

--- Maya/MayaExport/exporter.cpp
+++ Maya/MayaExport/exporter.cpp
@@ -2,7 +2,6 @@
 #include "mesh.h"
 #include "material.h"
 #include "Transform.h"
-#include "camera.h"
 #include "writeToFile.h"
 
 #include <vector>
@@ -22,11 +21,6 @@ MStatus Exporter::doIt(const MArgList& argList)
 
 	vector<TransformHeader> transfromHeaders;
 	vector<Transform> transformData;
-
-	// camera
-	Camera cam;
-	vector<CameraHeader> camera_header;
-	vector<camera> cameraVec;
 
 	while (!dagIt.isDone())
 	{
@@ -60,36 +54,18 @@ MStatus Exporter::doIt(const MArgList& argList)
 			{
 				
 			}
-
-			if (path.apiType() == MFn::kCamera)
-			{
-				MFnCamera mayaCamera(path);
-				camera camera;
-				CameraHeader camHeader;
-				status = cam.exportCamera(mayaCamera, camera, camHeader);
-
-				if (status != MS::kSuccess)
-				{
-					MGlobal::displayInfo("Failure at Camera::exportCamera()");
-				}
-				camera_header.push_back(camHeader);
-				cameraVec.push_back(camera);
-			}
 		}
 		dagIt.next(); // without this line, Maya will crash.
 	}
 
 	//Printing to files
 	WriteToFile output;
-	output.binaryFilePath("C://test.bin");
-	output.ASCIIFilePath("C://testASCII.txt");
+	output.binaryFilePath("c://test.bin");
+	output.ASCIIFilePath("c://testASCII.txt");
 	output.OpenFiles();
 	output.writeToFiles(&header, 1);
 	output.writeToFiles(&transfromHeaders[0], transfromHeaders.size());
 	output.writeToFiles(&transformData[0], transformData.size());
-
-	// camera
-	output.writeToFiles(&cameraVec[0], cameraVec.size());
 
 	output.CloseFiles();
 	return MStatus::kSuccess;
