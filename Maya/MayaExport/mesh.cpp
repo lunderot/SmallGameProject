50c00ed77348e12144ad43291bdf5319a18e2c37
dsdsd <sdsdsdsd>
2015-04-12 19:38:03 +0200

    mesh
    face 8 0 är fel. tangent, bi-tangents???

--- Maya/MayaExport/mesh.cpp
+++ Maya/MayaExport/mesh.cpp
@@ -18,6 +18,13 @@ MStatus Mesh::exportMaterial(MFnMesh& mesh, map<const char*, unsigned int>& mate
 MStatus Mesh::exportVertices(MFnMesh& mesh, meshStruct& meshes)
 {
 	MStatus status;
+	//MString cmd = "polyTriangulate -ch 1 " + mesh.name();
+	//MGlobal::executeCommand(cmd);
+
+	/*if (!MGlobal::executeCommand(cmd))
+	{
+		MGlobal::displayInfo("TRIANGLUATATATATATATATATATATATA");
+	}*/
 
 	unsigned int numVertices = mesh.numVertices(&status);
 	unsigned int numUVs = mesh.numUVs(&status);
@@ -74,7 +81,7 @@ MStatus Mesh::exportVertices(MFnMesh& mesh, meshStruct& meshes)
 
 	MFloatVectorArray Norm;
 	mesh.getNormals(Norm, MSpace::kWorld);
-	for (unsigned int i = 0; i < numNormals; i++)
+	for (unsigned int i = 0; i < Norm.length()/*numNormals*/; i++)
 	{
 		MGlobal::displayInfo(MString() + i + " | Norm: " + Norm[i].x + " " + Norm[i].y + " " + Norm[i].z);
 	}
@@ -112,9 +119,21 @@ MStatus Mesh::exportVertices(MFnMesh& mesh, meshStruct& meshes)
 	MObjectArray shaders;
 	MIntArray shaderIndicies;
 	//mesh.getConnectedShaders(instanceNumber, shaders, shaderIndicies);
-	MGlobal::displayInfo(MString() + shaders.length());
+	//Global::displayInfo(MString() + shaders.length());
 
-	MGlobal::displayInfo(MString() + "TriangleCount: " + indicies.length());
+	MIntArray normalcounts;
+	MIntArray testnormals;
+	mesh.getNormalIds(normalcounts, testnormals);
+	
+	int test2;
+	mesh.getPolygonUVid(7, 0, test2, NULL);
+	MGlobal::displayInfo(MString() + test2);
+	mesh.getPolygonUVid(8, 0, test2, NULL);
+	MGlobal::displayInfo(MString() + test2);
+	mesh.getPolygonUVid(9, 0, test2, NULL);
+	MGlobal::displayInfo(MString() + test2);
+	MGlobal::displayInfo(MString() + normalcounts.length() + " " + testnormals.length());
+	MGlobal::displayInfo(MString() + "TriangleCount: " + indicies.length()/3);
 	MGlobal::displayInfo(MString() + "Verticies: " + numVertices);
 	MGlobal::displayInfo(MString() + "UV total: " + numUVs);
 	MGlobal::displayInfo(MString() + "Normal total: " + numNormals);
@@ -122,31 +141,42 @@ MStatus Mesh::exportVertices(MFnMesh& mesh, meshStruct& meshes)
 	meshes.meshHeader.vertex_count = numVertices;
 	meshes.meshHeader.triangle_count = indicies.length() / 3;
 	meshes.vertices.resize(indicies.length());
-	
+
 	int NormControl = 0;
 	int NormCounter = 0;
 	int NormCounter1 = 0;
 	int store = 0;
 	for (unsigned int i = 0; i < indicies.length(); i++)
 	{
-		int tmp;
-		tmp = indicies[i];
-		meshes.vertices[i].position[0] = vertexArray[tmp].x;
-		meshes.vertices[i].position[1] = vertexArray[tmp].y;
-		meshes.vertices[i].position[2] = vertexArray[tmp].z;
+		int tmpPosition;
+		tmpPosition = indicies[i];
+		meshes.vertices[i].position[0] = vertexArray[tmpPosition].x;
+		meshes.vertices[i].position[1] = vertexArray[tmpPosition].y;
+		meshes.vertices[i].position[2] = vertexArray[tmpPosition].z;
 		//MGlobal::displayInfo(MString() + i + " | POSITION: " + meshes.vertices[i].position[0] + " " + meshes.vertices[i].position[1] + " " + meshes.vertices[i].position[2]);
 
-		meshes.vertices[i].uv[0] = uArray[0];
-		meshes.vertices[i].uv[1] = vArray[0];
+		int tmpNormal;
+		tmpNormal = testnormals[i];
+		meshes.vertices[i].normal[0] = Norm[tmpNormal].x;
+		meshes.vertices[i].normal[1] = Norm[tmpNormal].y;
+		meshes.vertices[i].normal[2] = Norm[tmpNormal].z;
 
-		meshes.vertices[i].normal[0] = Norm[NormCounter].x;
+		meshes.vertices[i].tangent[0] = tangent[tmpNormal].x;
+		meshes.vertices[i].tangent[1] = tangent[tmpNormal].y;
+		meshes.vertices[i].tangent[2] = tangent[tmpNormal].z;
+
+		meshes.vertices[i].bi_tangent[0] = biNormals[tmpNormal].x;
+		meshes.vertices[i].bi_tangent[1] = biNormals[tmpNormal].y;
+		meshes.vertices[i].bi_tangent[2] = biNormals[tmpNormal].z;
+
+		/*meshes.vertices[i].normal[0] = Norm[NormCounter].x;
 		meshes.vertices[i].normal[1] = Norm[NormCounter].y;
-		meshes.vertices[i].normal[2] = Norm[NormCounter].z;
+		meshes.vertices[i].normal[2] = Norm[NormCounter].z;*/
 		//MGlobal::displayInfo(MString() + i + " | NORMAL: " + meshes.vertices[i].normal[0] + " " + meshes.vertices[i].normal[1] + " " + meshes.vertices[i].normal[2]);
 		
-		MGlobal::displayInfo(MString() + "F: " + (tmp+1)/*(indicies[i]+1)*/ + " | " + " | " + (NormCounter+1));	
+		//MGlobal::displayInfo(MString() + "F: " + (tmp+1)/*(indicies[i]+1)*/ + " | " + " | " + (NormCounter+1));	
 		
-		if (NormControl != 2)
+		/*if (NormControl != 2)
 		{
 			NormCounter++;
 			NormControl++;
@@ -176,24 +206,66 @@ MStatus Mesh::exportVertices(MFnMesh& mesh, meshStruct& meshes)
 		if (NormCounter1 == 0)
 		{
 			NormCounter++;
-		}
+		}*/
 
 	}
-
-	/*for (unsigned int i = 0; i < numNormals; i++)
+	
+	for (unsigned int i = 0; i < indicies.length() / 3; i++)
 	{
-		meshes.vertices[i].normal[0] = Norm[i].x;
-		meshes.vertices[i].normal[1] = Norm[i].y;
-		meshes.vertices[i].normal[2] = Norm[i].z;
-		MGlobal::displayInfo(MString() + i + " | NORMAL: " + meshes.vertices[i].normal[0] + " " + meshes.vertices[i].normal[1] + " " + meshes.vertices[i].normal[2]);
-	}*/
+		int uvId;
+		for (unsigned int k = 0; k < 3; k++)
+		{
+			mesh.getPolygonUVid(i, k, uvId, NULL);
+			meshes.vertices[i].uv[0] = uArray[uvId];
+			meshes.vertices[i].uv[1] = vArray[uvId];
+			
+			mesh.getPolygonUVid(i, (k + 1), uvId, NULL);
+			meshes.vertices[i].uv[0] = uArray[uvId];
+			meshes.vertices[i].uv[1] = vArray[uvId];
 
-	//for (unsigned int i = 0; i < numUVs; i++)
-	//{
-	//	meshes.vertices[i].uv[0] = uArray[i];
-	//	meshes.vertices[i].uv[1] = vArray[i];
-	//	MGlobal::displayInfo(MString() + i + " | UV: " + meshes.vertices[i].uv[0] + " " + meshes.vertices[i].uv[1]);
-	//}
+			mesh.getPolygonUVid(i, (k + 2), uvId, NULL);
+			meshes.vertices[i].uv[0] = uArray[uvId];
+			meshes.vertices[i].uv[1] = vArray[uvId];
+		}
+	}
+
+	bool displayControl = true;
+	int displayCounter = 0;
+	int displayCheck = 1;
+	int displayUVid = 0;
+	int displayUVcounter = 0;
+	int displayUVresult;
+	for (unsigned int i = 0; i < indicies.length(); i++)
+	{
+		if (displayControl == true)
+		{
+			MGlobal::displayInfo(MString() + "F: " + displayCounter + "                    " + displayUVid);
+			displayControl = false;
+			displayCounter++;
+		}
+		mesh.getPolygonUVid(displayUVcounter, displayUVid, displayUVresult, NULL);
+		MGlobal::displayInfo(MString() + "VERTEX: " + indicies[i] + " | " + displayUVresult + " | " + testnormals[i]);
+		MGlobal::displayInfo(MString() + "POS: " + meshes.vertices[i].position[0] + " " + meshes.vertices[i].position[1] + " " + meshes.vertices[i].position[2]);
+		MGlobal::displayInfo(MString() + "UV: " + meshes.vertices[i].uv[0] + " " + meshes.vertices[i].uv[1]);
+		MGlobal::displayInfo(MString() + "NORM:       " + meshes.vertices[i].normal[0] + " " + meshes.vertices[i].normal[1] + " " + meshes.vertices[i].normal[2]);
+		MGlobal::displayInfo(MString() + "TANGENT:    " + meshes.vertices[i].tangent[0] + " " + meshes.vertices[i].tangent[1] + " " + meshes.vertices[i].tangent[2]);
+		MGlobal::displayInfo(MString() + "BI-TANGENT: " + meshes.vertices[i].bi_tangent[0] + " " + meshes.vertices[i].bi_tangent[1] + " " + meshes.vertices[i].bi_tangent[2]);
+		if (displayUVid == 3)
+		{
+			displayUVid = 0;
+			displayUVcounter++;
+		}
+		
+		if (displayCheck == 3)
+		{
+			MGlobal::displayInfo("                                  ");
+			displayControl = true;
+			displayCheck = 0;
+		}
+		displayCheck++;
+		displayUVid++;
+	}
+	MGlobal::displayInfo(MString() + displayUVcounter);
 
 	return MStatus::kSuccess;
 }
